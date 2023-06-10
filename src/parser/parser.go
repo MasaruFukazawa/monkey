b83ce0f320285b142b05ee84fa4ab894acf7756b
Masaru Fukazawa <upw.ponsuke@gmail.com>
2023-06-10 22:21:13 +0900

    2.4 let文の構文解析途中まで。 = より右辺の解析はまだ

---
+++ src/parser/parser.go
@@ -0,0 +1,194 @@
+/**
+ * パッケージ名: parser
+ * ファイル名: parser.go
+ * 概要: 構文解析器を実装する
+ */
+package parser
+
+import (
+	"fmt"
+
+	"github.com/MasaruFukazawa/monkey-lang/src/ast"
+	"github.com/MasaruFukazawa/monkey-lang/src/lexer"
+	"github.com/MasaruFukazawa/monkey-lang/src/token"
+)
+
+// 構文解析器を表す構造体
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+	errors    []string
+}
+
+/**
+ * 名前: New
+ * 処理: 構文解析器のポインタを返す
+ * 引数: *lexer.Lexer
+ * 戻値: *Parser
+ */
+func New(l *lexer.Lexer) *Parser {
+
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+/**
+ * 名前: Parser.nextToken
+ * 処理: 次のトークンを読み込む
+ * 引数: なし
+ * 戻値: なし
+ */
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+/**
+ * 名前: Parser.ParseProgram
+ * 処続: 構文解析を行う
+ * 引数: なし
+ * 戻値: *ast.Program
+ */
+func (p *Parser) ParseProgram() *ast.Program {
+
+	// programにast.Programのポインタを代入
+	program := &ast.Program{}
+
+	// program.Statementsにast.Statementを追加していく
+	program.Statements = []ast.Statement{}
+
+	// EOFに達するまで繰り返す
+	for p.curToken.Type != token.EOF {
+
+		// statementに構文解析結果を代入
+		stmt := p.parseStatement()
+
+		// stmtがnilでなければ、program.Statementsに追加
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		// 次のトークンへ進める
+		p.nextToken()
+
+	} // p.nextToken()
+
+	return program
+}
+
+/**
+ * 名前: Parser.parseStatement
+ * 処続: 構文解析を行う
+ * 引数: なし
+ * 戻値: ast.Statement
+ */
+func (p *Parser) parseStatement() ast.Statement {
+
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+/**
+ * 名前: Parser.parseLetStatement
+ * 処続: 構文解析を行う
+ */
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+
+	// letを持つast.LetStatementのポインタを生成
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	// 次のトークンがIDENTでなければnilを返す
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	// IDENTを持つast.Identifierを生成
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	// 次のトークンがASSIGNでなければnilを返す
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+/**
+ * 名前: Parser.curTokenIs
+ * 処続: 現在のトークンが引数のトークンと同じかどうかを返す
+ * 引数: token.TokenType
+ * 戻値: bool
+ */
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+/**
+ * 名前: Parser.peekTokenIs
+ * 処続: 次のトークンが引数のトークンと同じかどうかを返す
+ * 引数: token.TokenType
+ * 戻値: bool
+ */
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+/**
+ * 名前: Parser.expectPeek
+ * 処続: 次のトークンが引数のトークンと同じであれば、次のトークンへ進める
+ * 引数: token.TokenType
+ * 戻値: bool
+ */
+func (p *Parser) expectPeek(t token.TokenType) bool {
+
+	// 次のトークンが引数のトークンと同じであれば、次のトークンへ進める
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		// 次のトークンが引数のトークンと同じでなければ、エラーを追加
+		p.peekError(t)
+		return false
+	}
+}
+
+/**
+ * 名前: Parser.errors
+ * 処続: 構文解析中に発生したエラーを返す
+ * 引数: なし
+ * 戻値: []string
+ */
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+/**
+ * 名前: Parser.peekError
+ * 処続: 次のトークンが引数のトークンと同じでなければ、エラーを追加する
+ * 引数: token.TokenType
+ * 戻値: なし
+ */
+func (p *Parser) peekError(t token.TokenType) {
+
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+
+	// エラーを追加
+	p.errors = append(p.errors, msg)
+}
---
+++ src/parser/parser_test.go
@@ -0,0 +1,113 @@
+/**
+ * パッケージ名: parser
+ * ファイル名: parser_test.go
+ * 概要: parserのテストを実装する
+ */
+package parser
+
+import (
+	"testing"
+
+	"github.com/MasaruFukazawa/monkey-lang/src/ast"
+	"github.com/MasaruFukazawa/monkey-lang/src/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+
+	p := New(l)
+
+	program := p.ParseProgram()
+
+	// パーサーのエラーをチェック
+	checkParserErrors(t, p)
+
+	// programがnilでないことを確認
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	// program.Statementsの長さが3でないことを確認
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+/**
+ * 名前: testLetStatement
+ * 概要: LetStatementのテストを実装する
+ * 引数: t *testing.T, s Statement, name string
+ * 戻り値: bool
+ */
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+
+	if !ok {
+		t.Errorf("s not *LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
+}
+
+/**
+ * 名前: checkParserErrors
+ * 処理: パーサーのエラーをチェックする
+ * 引数:
+ * .. t *testing.T: テスト
+ * .. p *Parser: パーサー
+ * 戻り値:
+ */
+func checkParserErrors(t *testing.T, p *Parser) {
+
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
+}
