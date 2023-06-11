822a35484aaa11ac49e0e618a482fedf92783382
Masaru Fukazawa <upw.ponsuke@gmail.com>
2023-06-11 18:54:48 +0900

    2.6.4 ASTの準備 まで実装

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -6,6 +6,8 @@
 package ast
 
 import (
+	"bytes"
+
 	"github.com/MasaruFukazawa/monkey-lang/src/token"
 )
 
@@ -13,6 +15,10 @@ import (
 type Node interface {
 	// Nodeを継承する構造体は、TokenLiteral()メソッドを実装しなければならない
 	TokenLiteral() string
+
+	// デバック用に抽象構文木を文字列にして返す
+	// Nodeを継承する構造体は、String()メソッドを実装しなければならない
+	String() string
 }
 
 // 抽象構文木の文のインターフェース
@@ -56,6 +62,30 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+/**
+ * 名前: LetStatement.String
+ * 概要:
+ *	LET文のトークンリテラルを返す
+ *  Nodeインターフェースを満たす
+ */
+func (ls *LetStatement) String() string {
+
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	// Valueがnilでない場合
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 // Return文を表すノード
 type ReturnStatement struct {
 	Token       token.Token // 'return' トークン
@@ -87,6 +117,68 @@ func (rs *ReturnStatement) TokenLiteral() string {
 }
 
 /**
+ * 名前: ReturnStatement.String
+ * 概要:
+ *	Return文のトークンリテラルを返す
+ *  Nodeインターフェースを満たす
+ */
+func (rs *ReturnStatement) String() string {
+
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	// ReturnValueがnilでない場合
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// 式のノード
+type ExpressionStatement struct {
+	Token      token.Token // 式の最初のトークン
+	Expression Expression  // 式を保持するフィールド
+}
+
+/**
+ * 名前: ExpressionStatement.statementNode
+ * 概要:
+ *	ExpressionStatementのトークンリテラルを返す
+ *  Statementインターフェースを満たす
+ */
+func (es *ExpressionStatement) statementNode() {}
+
+/**
+ * 名前: ExpressionStatement.TokenLiteral
+ * 概要:
+ *	ExpressionStatementのトークンリテラルを返す
+ *  TokenLiteralインターフェースを満たす
+ */
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+/**
+ * 名前: ExpressionStatement.String
+ * 概要:
+ *	ExpressionStatementのトークンリテラルを返す
+ *  Nodeインターフェースを満たす
+ */
+func (es *ExpressionStatement) String() string {
+
+	// Expressionがnilの場合
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
+/**
  * 名前: Identifier.expressionNode
  * 概要:
  * 	識別子(変数名・関数名)のトークンリテラルを返す
@@ -102,6 +194,16 @@ func (i *Identifier) expressionNode() {}
  */
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+/**
+ * 名前: Identifier.String
+ * 概要:
+ *	識別子(変数名・関数名)のトークンリテラルを返す
+ *	Nodeインターフェースを満たす
+ */
+func (i *Identifier) String() string {
+	return i.Value
 }
 
 // プログラム全体を表すノード
@@ -123,4 +225,19 @@ func (p *Program) TokenLiteral() string {
 		// 空のプログラムの場合は空文字列を返す
 		return ""
 	}
+}
+
+/**
+ * 名前: String
+ * 概要: デバック用に抽象構文木を文字列にして返す
+ */
+func (p *Program) String() string {
+
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
---
+++ src/ast/ast_test.go
@@ -0,0 +1,37 @@
+/**
+ * パッケージ名: ast
+ * ファイル名: ast_test.go
+ * 概要: astのテストを実装する
+ */
+package ast
+
+import (
+	"testing"
+
+	"github.com/MasaruFukazawa/monkey-lang/src/token"
+)
+
+func TestString(t *testing.T) {
+
+	program := &Program{
+		Statements: []Statement{
+			// let myVar = anotherVar; の抽象構文木表現
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVal = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+
+}
