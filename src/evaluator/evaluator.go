dea401a9a9ce5b7f82bc82b7462b6946e74ba4b6
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-09-11 00:37:13 +0900

    3.5.1 整数リテラル の実装

---
+++ src/evaluator/evaluator.go
@@ -0,0 +1,40 @@
+package evaluator
+
+import (
+	"github.com/MasaruFukazawa/monkey-lang/src/ast"
+	"github.com/MasaruFukazawa/monkey-lang/src/object"
+)
+
+/**
+ * 関数名: Eval
+ * 処理: 引数で渡された抽象構文木を評価する
+ * 引数: 抽象構文木
+ * 戻値: 評価結果
+ */
+func Eval(node ast.Node) object.Object {
+
+	switch node := node.(type) {
+
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stms []ast.Statement) object.Object {
+
+	var result object.Object
+
+	for _, statement := range stms {
+		result = Eval(statement)
+	}
+
+	return result
+}
