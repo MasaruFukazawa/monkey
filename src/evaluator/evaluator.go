bad6939e7298fdb1d0916f0ba921d0b63f956819
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-09-15 16:27:38 +0900

    整数の演算を実装

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -36,6 +36,11 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalIntegerInfixExpression(node.Operator, left, right)
 	}
 
 	return nil
@@ -111,4 +116,35 @@ func evalPlusPrefixOperatorExpression(right object.Object) object.Object {
 	value := right.(*object.Integer).Value
 
 	return &object.Integer{Value: value}
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	default:
+		return NULL
+	}
+
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+
+	leftValue := left.(*object.Integer).Value
+	rightValue := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftValue + rightValue}
+	case "-":
+		return &object.Integer{Value: leftValue - rightValue}
+	case "*":
+		return &object.Integer{Value: leftValue * rightValue}
+	case "/":
+		return &object.Integer{Value: leftValue / rightValue}
+	default:
+		return NULL
+	}
+
 }
