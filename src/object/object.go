aec3877a6636ebc3bad1b905c12d532d26e5d040
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-12-29 09:22:04 +0900

    4.4.4 配列リテラルの評価 実装

--- src/object/object.go
+++ src/object/object.go
@@ -24,6 +24,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
 
 // オブジェクトの種類を定義する
@@ -162,4 +163,30 @@ func (b *Builtin) Type() ObjectType {
 
 func (b *Builtin) Inspect() string {
 	return "builtin function"
+}
+
+// 配列オブジェクトを表す構造体
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() ObjectType {
+	return ARRAY_OBJ
+}
+
+func (ao *Array) Inspect() string {
+
+	var out bytes.Buffer
+
+	elements := []string{}
+
+	for _, e := range ao.Elements {
+		elements = append(elements, e.Inspect())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
 }
